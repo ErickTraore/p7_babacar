408874ad8706bf8da6e8442b6932aa83cb8acbd4
ErickTraore <traoreerick@gmail.com>
2021-11-11 05:49:28 +0100

    graph page home

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,16 @@
 <template>
    <div class="nav">
-            <div class="nav__in" id="app">   
+        <div class="nav__in" id="app">   
                   <img alt="Vue logo" src="./assets/logo4.png"> 
-            </div>
-            <div class="nav__down">
-              <router-link to="/">Home</router-link> |
-              <router-link to="/profile">Profile</router-link> |  
-              <router-link v-if=logged to="/login">Login</router-link>  
-              <router-link v-else  to="/logout">Logout</router-link> | 
-              <router-link v-if= testBool  to="/compte">Admin</router-link> 
-
-           </div>
-                                                <router-view/>
-
+        </div>
+                <div class="nav__down">
+                  <router-link class="nav__down__first" to="/">Home</router-link> 
+                  <router-link class="nav__down__first" to="/profile">Profile</router-link>  
+                  <router-link class="nav__down__first" v-if=logged to="/login">Login</router-link>  
+                  <router-link class="nav__down__first" v-else  to="/logout">Logout</router-link> 
+                  <router-link class="nav__down__first" v-if= testBool  to="/compte">Admin</router-link> 
+                </div>
+        <router-view/>
   </div>
 </template>
 
@@ -60,20 +58,43 @@ export default {
 };
 </script>
 <style>
-
+.nav{
+  padding: 20px;
+}
 .nav__in{
-border: 3px solid white;
+  display:flex;
+ margin: 0 auto;
+border: 3px solid black;
+ width: 320px;
+height: 50px;
+border-radius: 30px;
 }
 .nav__down{
-   min-width: 300PX;
-  width: 100%;
-  height: 100%;
-border: 3px solid white;
+  padding: 20px;
+
 
 }
+.nav__down__first{
+border: 3px solid black;
+width: auto;
+height: auto;
+border-radius: 30px;
+border: 3px solid rgb(230, 122, 122);
+margin: 5px;
+padding: 5px;
+text-align: 16px;
+font-size: 10px;
+font-weight: bold;
+background: rgb(231, 216, 216);
+text-decoration:none;
+}
+
+
 img {
   width: 100%;
   height: 100%;
+  border-radius: 30px;
+  margin:  Oauto;
 }
 </style>
 
